b33f01149fafdb710cbea0cae14bd802da26b2f8
Amine <amine.tchitooss@gmail.com>
2023-03-06 19:29:06 +0100

    add recharts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
-import Slideshow from './lib';
-import VehicleGallery from './lib/test';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import Chart from "./charts";
 
 class App extends Component {
   render() {
-    const images = [
-      'images/slide_2.jpg',
-      'images/slide_3.jpg',
-      'images/slide_4.jpg',
-      'images/slide_5.jpg',
-      'images/slide_6.jpg'
-    ]
     return (
       <div className="App">
         <p>hello world</p>
-        {/* <Slideshow images={images} /> */}
-        <VehicleGallery  />
+        <Chart></Chart>
       </div>
     );
   }
